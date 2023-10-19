eabdf2c20fef607bde798f99fcbeb2517e469b34
mohamim360 <mohamim360@gmail.com>
2023-10-19 13:23:33 +0600

    Install stripe, stripe for react and create card element

--- src/Payment/Payment.jsx
+++ src/Payment/Payment.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import { Elements } from "@stripe/react-stripe-js";
+import React from "react";
+import CheckoutForm from "./CheckoutForm";
+import { loadStripe } from "@stripe/stripe-js";
+
+const stripePromise = loadStripe("");
 
 function Payment() {
-	return (
-		<>
-		Payment
-		</>
-	)
+  return (
+    <>
+      
+			<div>
+			Payment
+			<Elements stripe={stripePromise}>
+        <CheckoutForm></CheckoutForm>
+      </Elements>
+			</div>
+   
+    </>
+  );
 }
 
-export default Payment
+export default Payment;
